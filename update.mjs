b61d8e670f335babb307dfc39a6c8fcb7db85cf1
Maksym <m.v.korotenko@gmail.com>
2025-01-09 14:26:29 +0200

    Added update

---
+++ update.mjs
@@ -0,0 +1,29 @@
+import execa from 'execa';
+import path from 'path';
+
+// Вкажіть шлях до вашого проекту
+const projectPath = path.resolve(__dirname, '/home/Main-socket');
+
+// Функція для виконання команд
+async function runCommand(command, args) {
+  try {
+    const { stdout } = await execa(command, args, { cwd: projectPath });
+    console.log(`Stdout: ${stdout}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Отримання останніх змін з віддаленого репозиторію
+(async () => {
+  await runCommand('git', ['pull', 'origin', 'main']);
+  console.log('Files updated from GitHub');
+  
+  // Перезапуск 
+  await runCommand('sudo', ['systemctl', 'daemon-reload']);
+ console.log('Service restarted');
+
+  // Перезапуск сервісу
+  console.log('Service restarting...');
+  runCommand('sudo', ['systemctl', 'restart', 'main-socket.service']);
+})();
