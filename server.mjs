9aac103d1808a387aaa61ab56bc401c9e2b55fce
Maksym <m.v.korotenko@gmail.com>
2025-01-09 17:01:45 +0200

    Added update call

---
+++ server.mjs
@@ -0,0 +1,83 @@
+import { WebSocket } from 'ws';
+import { updateManager } from './update-manager.mjs';
+
+const port = 8080;
+
+const wsServer = new WebSocket.Server({ port });
+wsServer.binaryType = "arraybuffer";
+
+const decoder = new TextDecoder('utf-8');
+
+function tryDecode(data) {
+  try {
+    // return JSON.parse(data);
+    return decoder.decode(new Uint8Array(data));
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return undefined;
+  }
+}
+
+wsServer.on('connection', (ws) => {
+  console.log('Peer connection established');
+  ws.binaryType = "arraybuffer";
+
+  // ws.on('open', () => {
+  //   console.log('Peer connection opened');
+    setTimeout(() => {
+      console.log('Sending peer connection opened');
+      ws.send('Peer connection opened');
+    });
+  // });
+
+  ws.on('message', (message) => {
+    // const decoder = new TextDecoder('utf-8');
+    // decoder.decode(new Uint8Array(m))
+    const data = tryDecode(message);
+    if (data) {
+      console.log('Peer data:', data);
+      if (data.call) {
+        switch (data.call) {
+            case 'update':
+                console.log('Update call');
+                updateManager();
+                break;
+        }
+        return;
+      }
+    } else {
+      console.log('Peer message:', message);
+    }
+
+    // Розсилаємо повідомлення всім підключеним клієнтам, виключаючи відправника
+    wsServer.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Peer connection closed');
+    ws.send('Peer connection closed');
+    wsServer.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send('Some peer closed');
+      }
+    });
+  });
+
+  ws.on('error', (error) => {
+    console.error('Peer WebSocket error:', error);
+    ws.send('Peer WebSocket error:', error);
+  });
+
+  // ws.send('Connection established');
+  wsServer.clients.forEach((client) => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send('New peer connected');
+    }
+  });
+});
+
+console.log(`WebSocket server is running on port ${port}`);
---
+++ update-manager.mjs
@@ -0,0 +1,31 @@
+import { execa } from 'execa';
+
+// Вкажіть шлях до вашого проекту
+const projectPath = '/home/Main-socket';
+
+// Функція для виконання команд
+async function runCommand(command, args) {
+  try {
+    const { stdout } = await execa(command, args, { cwd: projectPath });
+    console.log(`Stdout: ${stdout}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+// Отримання останніх змін з віддаленого репозиторію
+export default async function updateManager() {
+  await runCommand('git', ['pull', 'origin', 'main']);
+  console.log('Files updated from GitHub');
+
+  await runCommand('npm', ['install']);
+  console.log('Updated npm packages');
+  
+  // Перезапуск 
+  await runCommand('sudo', ['systemctl', 'daemon-reload']);
+ console.log('Service restarted');
+
+  // Перезапуск сервісу
+  console.log('Service restarting...');
+  runCommand('sudo', ['systemctl', 'restart', 'main-socket.service']);
+};
